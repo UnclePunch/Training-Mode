318f6ab95601d36ad0419f83649976003d7dbc4d
Vin Bertinelli <vinbertinelli@gmail.com>
2018-11-14 22:14:24 -0500

    Show GALINT for laser lands

---
+++ ASM/Onscreen Display/Ledge Codes/GALINT/GALINT Laser Land.asm
@@ -0,0 +1,106 @@
+#To be inserted at 80082c40
+.macro branchl reg, address
+lis \reg, \address @h
+ori \reg,\reg,\address @l
+mtctr \reg
+bctrl
+.endm
+
+.macro branch reg, address
+lis \reg, \address @h
+ori \reg,\reg,\address @l
+mtctr \reg
+bctr
+.endm
+
+.macro load reg, address
+lis \reg, \address @h
+ori \reg, \reg, \address @l
+.endm
+
+.macro loadf regf,reg,address
+lis \reg, \address @h
+ori \reg, \reg, \address @l
+stw \reg,-0x4(sp)
+lfs \regf,-0x4(sp)
+.endm
+
+.macro backup
+mflr r0
+stw r0, 0x4(r1)
+stwu	r1,-0x100(r1)	# make space for 12 registers
+stmw  r3,0x8(r1)
+.endm
+
+.macro restore
+lmw  r3,0x8(r1)
+lwz r0, 0x104(r1)
+addi	r1,r1,0x100	# release the space
+mtlr r0
+.endm
+
+.macro intToFloat reg,reg2
+xoris    \reg,\reg,0x8000
+lis    r18,0x4330
+lfd    f16,-0x7470(rtoc)    # load magic number
+stw    r18,0(r2)
+stw    \reg,4(r2)
+lfd    \reg2,0(r2)
+fsubs    \reg2,\reg2,f16
+.endm
+
+.set ActionStateChange,0x800693ac
+.set HSD_Randi,0x80380580
+.set HSD_Randf,0x80380528
+.set Wait,0x8008a348
+.set Fall,0x800cc730
+.set TextCreateFunction,0x80005928
+
+.set entity,31
+.set playerdata,31
+.set player,30
+.set text,29
+.set textprop,28
+.set hitbool,27
+
+.set PrevASStart,0x23F0
+.set CurrentAS,0x10
+.set OneASAgo,PrevASStart+0x0
+.set TwoASAgo,PrevASStart+0x2
+.set ThreeASAgo,PrevASStart+0x4
+.set FourASAgo,PrevASStart+0x6
+.set FiveASAgo,PrevASStart+0x8
+.set SixASAgo,PrevASStart+0xA
+
+
+##########################################################
+## 804a1f5c -> 804a1fd4 = Static Stock Icon Text Struct ##
+## Is 0x80 long and is zero'd at the start              ##
+##  of every VS Match				                        ##
+## Store Text Info here                                 ##
+##########################################################
+
+backup
+
+#Search last 5 AS's for CliffWait
+  li  r5,0x0
+  lwz r6,0x2C(28)
+CliffWaitSearch:
+  mulli r3,r5,0x2
+  addi  r3,r3,PrevASStart
+  lhzx r3,r3,r6
+  cmpwi	r3,0xFD
+  beq	DisplayGALINT
+  addi r5,r5,1
+  cmpwi r5,5
+  ble CliffWaitSearch
+  b Moonwalk_Exit
+
+#Display GALINT
+DisplayGALINT:
+  mr  r3,r28
+  branchl	r12,0x80005514
+
+Moonwalk_Exit:
+restore
+mr	r3, r28
