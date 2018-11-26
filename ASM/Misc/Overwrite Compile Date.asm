dfaa13112f0be555852a4320a5df1b2707f50360
Vin Bertinelli <vinbertinelli@gmail.com>
2018-11-26 17:53:31 -0500

    Change version and compile date to .string directives
    Change version and compile date to use .string directives.

--- ASM/Misc/Overwrite Compile Date.asm
+++ ASM/Misc/Overwrite Compile Date.asm
@@ -1,9 +1,3 @@
 #To be inserted at 803ea6c8
-.long 0x44415445 
-.long 0x2058582f
-.long 0x59592f5a
-.long 0x5a5a5a20
-.long 0x2054494d
-.long 0x45204141
-.long 0x3a424200
-.long 0x00000000
+.string "DATE XX.YY.ZZZZ TIME AA.BB"
+.align 2
