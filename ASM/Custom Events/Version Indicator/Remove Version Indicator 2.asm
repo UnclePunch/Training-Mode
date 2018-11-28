a0fc86c949222fedb4f713aa318dbb476b436424
Vin Bertinelli <vinbertinelli@gmail.com>
2018-11-28 03:41:18 -0500

    Fix version indicator text remvoal bug
    remove text during both states of the event match menu load.
    
    if the event match is exited while still "loading", the text would not be removed. this was actually an old bug whose fix didnt get carried over to the geckotools method of assembling the mod.

---
+++ ASM/Custom Events/Version Indicator/Remove Version Indicator 2.asm
@@ -0,0 +1,63 @@
+#To be inserted at 8024e3b0
+.macro branchl reg, address
+lis \reg, \address @h
+ori \reg,\reg,\address @l
+mtctr \reg
+bctrl
+.endm
+
+.macro branch reg, address
+lis \reg, \address @h
+ori \reg,\reg,\address @l
+mtctr \reg
+bctr
+.endm
+
+.macro load reg, address
+lis \reg, \address @h
+ori \reg, \reg, \address @l
+.endm
+
+.macro loadf regf,reg,address
+lis \reg, \address @h
+ori \reg, \reg, \address @l
+stw \reg,-0x4(sp)
+lfs \regf,-0x4(sp)
+.endm
+
+.macro backup
+mflr r0
+stw r0, 0x4(r1)
+stwu	r1,-0x100(r1)	# make space for 12 registers
+stmw  r20,0x8(r1)
+.endm
+
+ .macro restore
+lmw  r20,0x8(r1)
+lwz r0, 0x104(r1)
+addi	r1,r1,0x100	# release the space
+mtlr r0
+.endm
+
+.macro intToFloat reg,reg2
+xoris    \reg,\reg,0x8000
+lis    r18,0x4330
+lfd    f16,-0x7470(rtoc)    # load magic number
+stw    r18,0(r2)
+stw    \reg,4(r2)
+lfd    \reg2,0(r2)
+fsubs    \reg2,\reg2,f16
+.endm
+
+.set entity,31
+.set player,31
+
+
+#GET STRUCT POINTER
+	lwz	r3, -0x4EB4 (r13)
+
+#REMOVE TEXT
+	branchl	r12,0x803a5cc4
+
+#Original Line
+lwz	r3, 0x0074 (r31)
