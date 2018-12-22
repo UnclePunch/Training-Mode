6f2dde2019f07ac254e6129fb5341793837ce7d5
Vin Bertinelli <vinbertinelli@gmail.com>
2018-12-21 23:02:02 -0500

    Change Global.h to Global.s
    ".s" is an assembly source file extension and Atom picks up on it and uses the PPC grammar.

--- ASM/Misc/Globals.s
+++ ASM/Misc/Globals.s
@@ -1,50 +1,3 @@
-.macro branchl reg, address
-lis \reg, \address @h
-ori \reg,\reg,\address @l
-mtctr \reg
-bctrl
-.endm
-
-.macro branch reg, address
-lis \reg, \address @h
-ori \reg,\reg,\address @l
-mtctr \reg
-bctr
-.endm
-
-.macro load reg, address
-lis \reg, \address @h
-ori \reg, \reg, \address @l
-.endm
-
-.macro backup
-mflr r0
-stw r0, 0x4(r1)
-stwu	r1,-0x100(r1)	# make space for 12 registers
-stmw  r20,0x8(r1)
-.endm
-
- .macro restore
-lmw  r20,0x8(r1)
-lwz r0, 0x104(r1)
-addi	r1,r1,0x100	# release the space
-mtlr r0
-.endm
-
-.macro backupall
-mflr r0
-stw r0, 0x4(r1)
-stwu	r1,-0x100(r1)	# make space for 12 registers
-stmw  r3,0x8(r1)
-.endm
-
-.macro restoreall
-lmw  r3,0x8(r1)
-lwz r0, 0x104(r1)
-addi	r1,r1,0x100	# release the space
-mtlr r0
-.endm
-
 #Debug Flag
 .set debug,0
 
@@ -143,3 +96,50 @@ mtlr r0
 
 #Custom Functions
 .set CheckIfCustomEvent,0x80005520
+
+.macro branchl reg, address
+lis \reg, \address @h
+ori \reg,\reg,\address @l
+mtctr \reg
+bctrl
+.endm
+
+.macro branch reg, address
+lis \reg, \address @h
+ori \reg,\reg,\address @l
+mtctr \reg
+bctr
+.endm
+
+.macro load reg, address
+lis \reg, \address @h
+ori \reg, \reg, \address @l
+.endm
+
+.macro backup
+mflr r0
+stw r0, 0x4(r1)
+stwu	r1,-0x100(r1)	# make space for 12 registers
+stmw  r20,0x8(r1)
+.endm
+
+ .macro restore
+lmw  r20,0x8(r1)
+lwz r0, 0x104(r1)
+addi	r1,r1,0x100	# release the space
+mtlr r0
+.endm
+
+.macro backupall
+mflr r0
+stw r0, 0x4(r1)
+stwu	r1,-0x100(r1)	# make space for 12 registers
+stmw  r3,0x8(r1)
+.endm
+
+.macro restoreall
+lmw  r3,0x8(r1)
+lwz r0, 0x104(r1)
+addi	r1,r1,0x100	# release the space
+mtlr r0
+.endm
