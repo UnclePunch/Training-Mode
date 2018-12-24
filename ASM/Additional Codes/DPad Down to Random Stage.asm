fa3e8ef432d38b2367e2deb5fbd2363f3ae6fe7d
Vin Bertinelli <vinbertinelli@gmail.com>
2018-12-23 20:36:20 -0500

    Fix DPad down for random stage rewrite bug
    fixed bug where the code wouldnt disable random stage when pressing start.

--- ASM/Additional Codes/DPad Down to Random Stage.asm
+++ ASM/Additional Codes/DPad Down to Random Stage.asm
@@ -6,11 +6,12 @@ load r4,0x8045BF17
 
 #Check for DPad Down
   rlwinm.	r0, r7, 0, 29, 29
-  beq Exit
+  beq CheckForStart
   li r3,0x1
   stb r3,0x0(r4)
   branch r12,0x80263264
 
+CheckForStart:
 #Check for Start
   rlwinm.	r0, r7, 0, 19, 19
   beq Exit
