f0844d00109e3dcc6abfb4e8bae02959eca3388e
Vin Bertinelli <vinbertinelli@gmail.com>
2019-02-14 04:20:03 -0500

    cleanup wavedash OSD code

--- ASM/Onscreen Display/Wavedash Display/Wavedash Timing Display.asm
+++ ASM/Onscreen Display/Wavedash Display/Wavedash Timing Display.asm
@@ -1,46 +1,38 @@
 #To be inserted at 80099d7c
 .include "../../Globals.s"
 
-.set entity,31
-.set playerdata,31
-.set player,30
-.set text,29
-.set textprop,28
+.set playerdata,29
+.set player,27
+.set text,28
 
-##########################################################
-## 804a1f5c -> 804a1fd4 = Static Stock Icon Text Struct ##
-## Is 0x80 long and is zero'd at the start              ##
-##  of every VS Match				                        ##
-## Store Text Info here                                 ##
-##########################################################
+backup
 
-backupall
-
+#Get Pointers
 	mr	player,r3
-	lwz	playerdata,0x2c(player)
+	lwz playerdata,0x2C(player)
 
-	#CHECK IF ENABLED
+
+#CHECK IF OSD IS ENABLED
 	li	r0,OSD.Wavedash			#wavedash ID
-	#lwz	r4,-0xdbc(rtoc)			#get frame data toggle bits
 	lwz	r4,-0x77C0(r13)
 	lwz	r4,0x1F24(r4)
 	li	r3, 1
 	slw	r0, r3, r0
 	and.	r0, r0, r4
-	beq	Moonwalk_Exit
+	beq	Injection_Exit
 
-	CheckForFollower:
+#Check if player is a subcharacter
 	mr	r3,playerdata
 	branchl	r12,0x80005510
 	cmpwi	r3,0x1
-	beq	Moonwalk_Exit
+	beq	Injection_Exit
 
-	#CHECK IF IT WAS A WAVEDASH (compare with 20)
-	lfs	f1,0x894(playerdata)
-	lfs	f2,-0x7fac(rtoc)			#get fp 20
-	fcmpo	cr0,f1,f2
-	bgt	Moonwalk_Exit
+#CHECK IF IT WAS A WAVEDASH (landed within 20 frames)
+	lhz	r3,FramesinCurrentAS(playerdata)
+	cmpwi r3,20
+	bgt	Injection_Exit
 
+#Create OSD Popup
 	mr	r3,playerdata			#backup playerdata pointer
 	li	r4,60			#display for 60 frames
 	li	r5,0			#Area to Display (0-2)
@@ -49,73 +41,79 @@ backupall
 
 	mr	text,r3			#backup text pointer
 
-	#Display Wavedash Frame
-		mr 	r3,r29			#text pointer
-		bl	TextASCII
-		mflr 	r4			#get ASCII to print
-		lfs	f1,0x894(playerdata)
-		fctiwz	f1,f1
-		stfd	f1,0xF0(sp)
-		lwz	r5,0xF4(sp)
-		lfs	f1, -0x37B4 (rtoc)			#default text X/Y
-		lfs	f2, -0x37B4 (rtoc)			#default text X/Y
-		branchl r12,0x803a6b98
+###########################
+## Print Wavedash Timing ##
+###########################
 
-	#Check if frame perfect
-		lbz	r4,0x680(playerdata)
-		cmpwi	r4,0x0
-		bne DisplayWavedashAngle
-	#Change Color
-		mr	r4,r3				#subtext id
-		mr 	r3,r29			#text pointer
-		load	r5,0x8dff6eff
-		stw	r5,0xF0(sp)
-		addi r5,sp,0xF0
-		branchl r12,0x803a74f0
+#Write out top line
+	mr 	r3,text			#text pointer
+	bl	TextASCII
+	mflr 	r4			#get ASCII to print
+	lhz	r5,FramesinCurrentAS(playerdata)
+	addi r5,r5,1								#1-index number
+	lfs	f1, -0x37B4 (rtoc)			#default text X/Y
+	lfs	f2, -0x37B4 (rtoc)			#default text X/Y
+	branchl r12,0x803a6b98
 
-	DisplayWavedashAngle:
-	#Display Wavedash Angle
-		mr 	r3,r29			#text pointer
-		bl	TextASCII2
-		mflr 	r4			#get ASCII to print
-		lfs f3,AirdodgeAngle(playerdata)
-		lfs	f1, -0x37B4 (rtoc)			#default text X/Y
-		lfs	f2, -0x37B0 (rtoc)			#shift down on Y axis
-		crset 6
-		branchl r12,0x803a6b98
+#Check if frame perfect
+	lbz	r4,0x680(playerdata)
+	cmpwi	r4,0x0
+	bne DisplayWavedashAngle
+#Change Color
+	mr	r4,r3				#subtext id
+	mr 	r3,text			#text pointer
+	load	r5,0x8dff6eff
+	stw	r5,0xF0(sp)
+	addi r5,sp,0xF0
+	branchl r12,0x803a74f0
 
-	#Check Angle
-		lfs f1,AirdodgeAngle(playerdata)
-		bl	Floats
-		mflr r4
-	#Check For Perfect Angle
-		lfs f2,0x0(r4)
-		fcmpo cr0,f1,f2
-		beq PerfectAngle
-	#Check For OK Angle
-		lfs f2,0x4(r4)
-		fcmpo cr0,f1,f2
-		bgt Moonwalk_Exit
-		lfs f2,0x8(r4)
-		fcmpo cr0,f1,f2
-		blt Moonwalk_Exit
-	OKAngle:
-		load	r5,0xfff000ff
-		stw	r5,0xF0(sp)
-		b ChangeAngleColor
-	PerfectAngle:
-		load	r5,0x8dff6eff
-		stw	r5,0xF0(sp)
-		b ChangeAngleColor
-	ChangeAngleColor:
-	#Change Color
-		mr	r4,r3				#subtext id
-		mr 	r3,r29			#text pointer
-		addi r5,sp,0xF0
-		branchl r12,0x803a74f0
+###########################
+## Print Wavedash Angle  ##
+###########################
+
+DisplayWavedashAngle:
+#Display Wavedash Angle
+	mr 	r3,text			#text pointer
+	bl	TextASCII2
+	mflr 	r4			#get ASCII to print
+	lfs f3,AirdodgeAngle(playerdata)
+	lfs	f1, -0x37B4 (rtoc)			#default text X/Y
+	lfs	f2, -0x37B0 (rtoc)			#shift down on Y axis
+	crset 6
+	branchl r12,0x803a6b98
+
+#Check Angle
+	lfs f1,AirdodgeAngle(playerdata)
+	bl	Floats
+	mflr r4
+#Check For Perfect Angle
+	lfs f2,0x0(r4)
+	fcmpo cr0,f1,f2
+	beq PerfectAngle
+#Check For OK Angle
+	lfs f2,0x4(r4)
+	fcmpo cr0,f1,f2
+	bgt Injection_Exit
+	lfs f2,0x8(r4)
+	fcmpo cr0,f1,f2
+	blt Injection_Exit
+OKAngle:
+	load	r5,0xfff000ff
+	stw	r5,0xF0(sp)
+	b ChangeAngleColor
+PerfectAngle:
+	load	r5,0x8dff6eff
+	stw	r5,0xF0(sp)
+	b ChangeAngleColor
+ChangeAngleColor:
+#Change Color
+	mr	r4,r3				#subtext id
+	mr 	r3,text			#text pointer
+	addi r5,sp,0xF0
+	branchl r12,0x803a74f0
 
 
-b Moonwalk_Exit
+b Injection_Exit
 
 #########################
 
@@ -137,7 +135,8 @@ blrl
 
 ##############################
 
-Moonwalk_Exit:
-restoreall
+Injection_Exit:
+mr	r3,player
+restore
 
 lwz	r4, -0x514C (r13)
