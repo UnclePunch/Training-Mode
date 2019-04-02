3078c757db97ba17fd3d805f250a9d1a50dac958
Vin Bertinelli <vinbertinelli@gmail.com>
2019-04-02 01:40:17 -0400

    salty runback in all melee modes

--- ASM/Additional Codes/Hold A+B for Salty Runback + Hold A+X for Random Stage + Skip Result Screen.asm
+++ ASM/Additional Codes/Hold A+B for Salty Runback + Hold A+X for Random Stage + Skip Result Screen.asm
@@ -1,9 +1,5 @@
-#To be inserted at 801B15E4
+#To be inserted at 801a5b14
 .include "../Globals.s"
-
-addi	r30, r3, 0
-backup
-mr r28,r5
 
 #Get all players inputs
   li  r3,4
@@ -19,17 +15,17 @@ mr r28,r5
   b LoadCSS
 
 Runback:
-  li r4,0x2 #reload match scene
+  li r27,0x2 #reload match scene
   b exit
 
 RandomStage:
   branchl r12,0x802599EC #get random stage ID
 
-  #convert SSS ID to internal stage ID
-    load r4,0x803f06D0   #load stage ID table
-    mulli	r3, r3, 28     #stage ID to offset
-    add r4,r4,r3         #add to start of table
-    lbz r3,0xB(r4)       #get internal stage ID
+#convert SSS ID to internal stage ID
+  load r4,0x803f06D0   #load stage ID table
+  mulli	r3, r3, 28     #stage ID to offset
+  add r4,r4,r3         #add to start of table
+  lbz r3,0xB(r4)       #get internal stage ID
 
   load r4,0x8045AC64     #load VS match struct
 
@@ -39,13 +35,12 @@ RandomStage:
   load r4,0x8043207c         #Preload Cache
   stw r3,0xC(r4)             #Store to Preload Cache
 */
-  li r4,0x2                  #reload match scene
+  li r27,0x2                  #reload match scene
   b exit
 
 LoadCSS:
-  li r4,0x0  #load CSS
+  li r27,0x0  #load CSS
 
 exit:
-mr r3,r30
-mr r5,r28
-restore
+Original:
+  li	r29, 0
