316561ccc225b2e8ea98eb861e6cb31b5c90a887
Vin Bertinelli <vinbertinelli@gmail.com>
2019-06-22 06:59:03 -0400

    adjust menu navigation deadzone
    now that left and right changes pages, the 0.375 deadzone made it really easy for up and down analog presses to bleed into the X axis, causing the page to change.
    
    deadzone is now 0.5, is now very hard to bleed into the X axis while still requiring a similar amount of force (for me at least)

---
+++ ASM/Misc/Adjust Menu Navigation Deadzone/Adjust Menu Navigation Deadzone Boot.asm
@@ -0,0 +1,9 @@
+#To be inserted at 80406dc0
+.include "../../Globals.s"
+
+.set Deadzone,40
+
+.byte 00
+.byte Deadzone
+.byte 00
+.byte 00
---
+++ ASM/Misc/Adjust Menu Navigation Deadzone/Adjust Menu Navigation Deadzone.asm
@@ -0,0 +1,9 @@
+#To be inserted at 804c1f8c
+.include "../../Globals.s"
+
+.set Deadzone,40
+
+.byte 00
+.byte Deadzone
+.byte 00
+.byte 00
