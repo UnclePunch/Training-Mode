e2422117ef45b52ca86127dd182986f4791ea321
Vin Bertinelli <vinbertinelli@gmail.com>
2019-07-04 00:48:41 -0400

    only show controller values in event mode

--- ASM/Misc/Display Stick Info When Paused/Create Text on Pause.asm
+++ ASM/Misc/Display Stick Info When Paused/Create Text on Pause.asm
@@ -1,78 +1,5 @@
 #To be inserted at 801a10e8
-.macro branchl reg, address
-lis \reg, \address @h
-ori \reg,\reg,\address @l
-mtctr \reg
-bctrl
-.endm
-
-.macro branch reg, address
-lis \reg, \address @h
-ori \reg,\reg,\address @l
-mtctr \reg
-bctr
-.endm
-
-.macro load reg, address
-lis \reg, \address @h
-ori \reg, \reg, \address @l
-.endm
-
-.macro loadf regf,reg,address
-lis \reg, \address @h
-ori \reg, \reg, \address @l
-stw \reg,-0x4(sp)
-lfs \regf,-0x4(sp)
-.endm
-
-.macro backup
-mflr r0
-stw r0, 0x4(r1)
-stwu	r1,-0x100(r1)	# make space for 12 registers
-stmw  r20,0x8(r1)
-.endm
-
- .macro restore
-lmw  r20,0x8(r1)
-lwz r0, 0x104(r1)
-addi	r1,r1,0x100	# release the space
-mtlr r0
-.endm
-
-.macro intToFloat reg,reg2
-xoris    \reg,\reg,0x8000
-lis    r18,0x4330
-lfd    f16,-0x7470(rtoc)    # load magic number
-stw    r18,0(r2)
-stw    \reg,4(r2)
-lfd    \reg2,0(r2)
-fsubs    \reg2,\reg2,f16
-.endm
-
-.set ActionStateChange,0x800693ac
-.set HSD_Randi,0x80380580
-.set HSD_Randf,0x80380528
-.set Wait,0x8008a348
-.set Fall,0x800cc730
-.set TextCreateFunction,0x80005928
-
-.set entity,31
-.set playerdata,31
-.set player,30
-.set text,29
-.set textprop,28
-.set hitbool,27
-
-.set PrevASStart,0x23F0
-.set CurrentAS,0x10
-.set OneASAgo,PrevASStart+(0*0x2)
-.set TwoASAgo,PrevASStart+(1*0x2)
-.set ThreeASAgo,PrevASStart+(2*0x2)
-.set FourASAgo,PrevASStart+(3*0x2)
-.set FiveASAgo,PrevASStart+(4*0x2)
-.set SixASAgo,PrevASStart+(5*0x2)
-
-.set FFVar,0x240C
+.include "../../Globals.s"
 
 .set text,31
 .set textproperties,30
@@ -81,6 +8,12 @@ fsubs    \reg2,\reg2,f16
 .set GObj,31
 
 backup
+
+#Only in Event Mode
+  load r3,SceneController
+  lbz r3,Scene.CurrentMajor(r3)
+  cmpwi r3,Scene.EventMode
+  bne Exit
 
 load staticTextData,0x804a1f58
 
--- ASM/Misc/Display Stick Info When Paused/Remove Text on UnPause.asm
+++ ASM/Misc/Display Stick Info When Paused/Remove Text on UnPause.asm
@@ -1,78 +1,13 @@
 #To be inserted at 801a1124
-.macro branchl reg, address
-lis \reg, \address @h
-ori \reg,\reg,\address @l
-mtctr \reg
-bctrl
-.endm
-
-.macro branch reg, address
-lis \reg, \address @h
-ori \reg,\reg,\address @l
-mtctr \reg
-bctr
-.endm
-
-.macro load reg, address
-lis \reg, \address @h
-ori \reg, \reg, \address @l
-.endm
-
-.macro loadf regf,reg,address
-lis \reg, \address @h
-ori \reg, \reg, \address @l
-stw \reg,-0x4(sp)
-lfs \regf,-0x4(sp)
-.endm
-
-.macro backup
-mflr r0
-stw r0, 0x4(r1)
-stwu	r1,-0x100(r1)	# make space for 12 registers
-stmw  r20,0x8(r1)
-.endm
-
- .macro restore
-lmw  r20,0x8(r1)
-lwz r0, 0x104(r1)
-addi	r1,r1,0x100	# release the space
-mtlr r0
-.endm
-
-.macro intToFloat reg,reg2
-xoris    \reg,\reg,0x8000
-lis    r18,0x4330
-lfd    f16,-0x7470(rtoc)    # load magic number
-stw    r18,0(r2)
-stw    \reg,4(r2)
-lfd    \reg2,0(r2)
-fsubs    \reg2,\reg2,f16
-.endm
-
-.set ActionStateChange,0x800693ac
-.set HSD_Randi,0x80380580
-.set HSD_Randf,0x80380528
-.set Wait,0x8008a348
-.set Fall,0x800cc730
-.set TextCreateFunction,0x80005928
-
-.set entity,31
-.set playerdata,31
-.set player,30
-.set text,29
-.set textprop,28
-.set hitbool,27
-
-.set PrevASStart,0x23F0
-.set CurrentAS,0x10
-.set OneASAgo,PrevASStart+(0*0x2)
-.set TwoASAgo,PrevASStart+(1*0x2)
-.set ThreeASAgo,PrevASStart+(2*0x2)
-.set FourASAgo,PrevASStart+(3*0x2)
-.set FiveASAgo,PrevASStart+(4*0x2)
-.set SixASAgo,PrevASStart+(5*0x2)
+.include "../../Globals.s"
 
 .set FFVar,0x240C
+
+#Only in Event Mode
+  load r3,SceneController
+  lbz r3,Scene.CurrentMajor(r3)
+  cmpwi r3,Scene.EventMode
+  bne Exit
 
 #Get Text Struct
   load r3,0x804a1f58
@@ -90,5 +25,6 @@ fsubs    \reg2,\reg2,f16
 #Remove GObj
   branchl r12,0x80390228
 
+Exit:
 #Original Codeline
   lwz	r0, 0x000C (sp)
