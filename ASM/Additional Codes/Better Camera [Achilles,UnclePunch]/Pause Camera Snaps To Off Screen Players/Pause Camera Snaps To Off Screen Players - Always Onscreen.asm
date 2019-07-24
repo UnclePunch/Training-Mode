2e7d7664632c268377fef968e25287e0219a51c3
Vin Bertinelli <vinbertinelli@gmail.com>
2019-07-23 21:21:04 -0400

    fix pause snap crash
    crashed when non-p1 player paused

--- ASM/Additional Codes/Better Camera [Achilles,UnclePunch]/Pause Camera Snaps To Off Screen Players/Pause Camera Snaps To Off Screen Players - Always Onscreen.asm
+++ ASM/Additional Codes/Better Camera [Achilles,UnclePunch]/Pause Camera Snaps To Off Screen Players/Pause Camera Snaps To Off Screen Players - Always Onscreen.asm
@@ -1,9 +1,36 @@
 #To be inserted at 8002cf34
 .include "../../../Globals.s"
 
-#Check if player is alive
+.set REG_Pauser,31
+.set REG_Count,30
+
+backup
+
+#Get player port who paused
   load r3,0x8046b6a0
-  lbz r3,0x1(r3)
+  lbz REG_Pauser,0x1(r3)
+
+#Search all players for this port
+  li  REG_Count,0
+Loop:
+  mr  r3,REG_Count
+  branchl r12,PlayerBlock_LoadMainCharDataOffset
+  cmpwi r3,0
+  beq IncLoop
+#Check their port to see if it matches
+  mr  r3,REG_Count
+  branchl r12,0x8003345c
+  cmpw  r3,REG_Pauser
+  beq FoundPauser
+IncLoop:
+  addi  REG_Count,REG_Count,1
+  cmpwi REG_Count,4
+  blt Loop
+  b Dead
+
+FoundPauser:
+#Check if player is alive
+  mr  r3,REG_Count
   branchl r12,PlayerBlock_LoadMainCharDataOffset
   lwz r3,0x2C(r3)
   lbz r3,0x221F(r3)
@@ -16,3 +43,4 @@ Alive:
 Dead:
   li  r3,1
 Exit:
+  restore
