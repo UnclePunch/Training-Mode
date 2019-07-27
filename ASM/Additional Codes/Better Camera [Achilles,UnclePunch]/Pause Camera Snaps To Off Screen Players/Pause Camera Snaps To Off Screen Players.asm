ee4b60820c38d74f895ea29ff1bdf4e337c3cac7
Vin Bertinelli <vinbertinelli@gmail.com>
2019-07-26 23:59:01 -0400

    fix pause snap crash again
    this time for real

--- ASM/Additional Codes/Better Camera [Achilles,UnclePunch]/Pause Camera Snaps To Off Screen Players/Pause Camera Snaps To Off Screen Players.asm
+++ ASM/Additional Codes/Better Camera [Achilles,UnclePunch]/Pause Camera Snaps To Off Screen Players/Pause Camera Snaps To Off Screen Players.asm
@@ -1,17 +1,47 @@
 #To be inserted at 8002c5f0
 .include "../../../Globals.s"
 
-#Check if player is alive
+
+.set REG_Pauser,31
+.set REG_Count,30
+
+backup
+
+#Get player port who paused
   load r3,0x8046b6a0
-  lbz r3,0x1(r3)
+  lbz REG_Pauser,0x1(r3)
+
+#Search all players for this port
+  li  REG_Count,0
+Loop:
+  mr  r3,REG_Count
+  branchl r12,PlayerBlock_LoadMainCharDataOffset
+  cmpwi r3,0
+  beq IncLoop
+#Check their port to see if it matches
+  mr  r3,REG_Count
+  branchl r12,0x8003345c
+  cmpw  r3,REG_Pauser
+  beq FoundPauser
+IncLoop:
+  addi  REG_Count,REG_Count,1
+  cmpwi REG_Count,4
+  blt Loop
+  b Dead
+
+FoundPauser:
+#Check if player is alive
+  mr  r3,REG_Count
   branchl r12,PlayerBlock_LoadMainCharDataOffset
   lwz r3,0x2C(r3)
   lbz r3,0x221F(r3)
   rlwinm. r3,r3,0,25,25
-  bne Original
+  bne Dead
 
-Skip:
+Alive:
+  restore
   branch r12,0x8002c644
-
-Original:
+Dead:
+Exit:
+  restore
   lfs	f0, 0x000C (r26)
