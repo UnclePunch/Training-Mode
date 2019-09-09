b67eac0eca74157f945be71048346311333cd135
Vin Bertinelli <vinbertinelli@gmail.com>
2019-09-09 16:06:17 -0400

    update neutral spawns

--- ASM/Additional Codes/Neutral Spawn.asm
+++ ASM/Additional Codes/Neutral Spawn.asm
@@ -1,144 +1,302 @@
-#To be inserted at 8016e48c
+#To be inserted at 8016e510
 .include "../Globals.s"
 
-.set PlayerSlot,28
-.set StaticBlock,27
-.set SpawnTable,26
-.set SpawnOrder,25
-.set SpawnID,24
-.set FacingDirection,23
+.set MatchInfo,31
+.set SpawnTable,30
+.set REG_PlayerSlot,28
 
 backup
 
 #Don't run in singleplayer
   branchl r12,0x8016b41c
   cmpwi r3,0
-  bne Original
+  bne Exit
 #Don't run for players 5 and 6
-  cmpwi PlayerSlot,5
-  bge Original
+  cmpwi REG_PlayerSlot,5
+  bge Exit
 
-#Get static block
-  mr  r3,PlayerSlot
-  branchl r12,Playerblock_LoadStaticBlock
-  mr  StaticBlock,r3
+#Check if teams
+  lbz	r3, 0x24D0 (MatchInfo)
+  cmpwi r3,1
+  beq isTeams
+
+#region isSingles
+isSingles:
+#Get this players spawn order
+.set LoopCount,26
+.set SpawnOrder,25
+  li  SpawnOrder,0
+  li  LoopCount,0
+isSingles_Loop:
+#Load slot type
+  mr  r3,LoopCount
+  branchl r12,PlayerBlock_LoadSlotType
+  cmpwi r3,0x3
+  beq isSingles_IncLoop      #If =3, no player present
+#Player is present, check if this is the one were looking for
+  cmpw REG_PlayerSlot,LoopCount
+  beq isSingles_Exit
+#Increment offset
+  addi  SpawnOrder,SpawnOrder,1
+isSingles_IncLoop:
+  addi LoopCount,LoopCount,1
+  cmpwi LoopCount,4
+  ble isSingles_Loop
+isSingles_Exit:
+  mr  r3,REG_PlayerSlot
+  mr  r4,SpawnOrder
+  lbz	r5, 0x24D0 (MatchInfo)
+  bl  SetSpawn
+  b Exit
+#endregion
+
+#region isTeams
+isTeams:
+CheckIf2v2:
+.set REG_TeamID,26
+  li  REG_TeamID,0
+CheckIf2v2_Loop:
+#Check how many players are on this team
+  GetTeamCount:
+  .set REG_Count,25
+  .set REG_TeamMembers,24
+    li  REG_TeamMembers,0
+    li  REG_Count,0
+  GetTeamCount_Loop:
+  #Load slot type
+    mr  r3,REG_Count
+    branchl r12,PlayerBlock_LoadSlotType
+    cmpwi r3,0x3
+    beq GetTeamCount_IncLoop      #If =3, no player present
+  #Get Team
+    mr  r3,REG_Count
+    branchl r12,0x80033370
+    cmpw  r3,REG_TeamID
+    bne GetTeamCount_IncLoop
+  #Increment team members
+    addi  REG_TeamMembers,REG_TeamMembers,1
+  GetTeamCount_IncLoop:
+    addi  REG_Count,REG_Count,1
+    cmpwi REG_Count,4
+    blt GetTeamCount_Loop
+#If =1 or >2, exit
+  cmpwi REG_TeamMembers,1
+  beq Exit
+  cmpwi REG_TeamMembers,2
+  bgt Exit
+CheckIf2v2_IncLoop:
+  addi  REG_TeamID,REG_TeamID,1
+  cmpwi REG_TeamID,3
+  blt CheckIf2v2_Loop
+
+
+#Create an array
+CreateTeamArray:
+.set REG_TeamID,25
+.set REG_TeamArray,26
+.set REG_ArraySize,24
+  li  REG_TeamID,0
+  addi  REG_TeamArray,sp,0x80
+  li  REG_ArraySize,0
+CreateTeamArray_Loop:
+#Check how many players are on this team
+  CheckTeam:
+  .set REG_Count,23
+  .set REG_TeamMembers,22
+    li  REG_TeamMembers,0
+    li  REG_Count,0
+  CheckTeam_Loop:
+  #Load slot type
+    mr  r3,REG_Count
+    branchl r12,PlayerBlock_LoadSlotType
+    cmpwi r3,0x3
+    beq CheckTeam_IncLoop      #If =3, no player present
+  #Get Team
+    mr  r3,REG_Count
+    branchl r12,0x80033370
+    cmpw  r3,REG_TeamID
+    bne CheckTeam_IncLoop
+  #Add to array
+    stbx  REG_Count,REG_ArraySize,REG_TeamArray
+    addi  REG_ArraySize,REG_ArraySize,1
+  CheckTeam_IncLoop:
+    addi  REG_Count,REG_Count,1
+    cmpwi REG_Count,4
+    blt CheckTeam_Loop
+CreateTeamArray_IncLoop:
+  addi  REG_TeamID,REG_TeamID,1
+  cmpwi REG_TeamID,3
+  blt CreateTeamArray_Loop
+
+#Now search for this players ID
+SearchForPlayerID:
+.set  REG_Count,25
+  li  REG_Count,0
+SearchForPlayerID_Loop:
+  lbzx  r3,REG_Count,REG_TeamArray
+  cmpw  r3,REG_PlayerSlot
+  beq SearchForPlayerID_Exit
+SearchForPlayerID_IncLoop:
+  addi  REG_Count,REG_Count,1
+  cmpwi REG_Count,4
+  blt SearchForPlayerID_Loop
+SearchForPlayerID_Exit:
+  mr  r3,REG_PlayerSlot
+  mr  r4,REG_Count
+  lbz	r5, 0x24D0 (MatchInfo)
+  bl  SetSpawn
+  b Exit
+#endregion
+
+#region SetSpawn
+SetSpawn:
+.set  REG_PlayerSlot,31
+.set  REG_SpawnID,30
+.set  REG_IsTeams,29
+.set  REG_SpawnTable,28
+
+#Init
+  backup
+  mr  REG_PlayerSlot,r3
+  mr  REG_SpawnID,r4
+  mr  REG_IsTeams,r5
 
 #Get Neutral Spawn Table
   bl NeutralSpawnTable
-  mflr SpawnTable
-
-#Get this players spawn order
-  mr  r3,PlayerSlot
-  bl  GetSpawnOrder
-  mr  SpawnOrder,r3
+  mflr REG_SpawnTable
 
 #Get stage ID
   lwz		r6,-0x6CB8 (r13)
   li    r5,0
 #Get stage's spawn info
-StageSearchLoop:
-  mulli r4,r5,0x9         #table length
-  add   r4,r4,SpawnTable  #get stage start
-  lbz   r3,0x0(r4)        #get stage ID
-  extsb r3,r3
-  cmpwi r3,-1             #check for end of table
-  beq   Original
+SetSpawn_StageSearchLoop:
+  lwz   r3,StageID(REG_SpawnTable)            #get stage ID
+  cmpwi r3,-1                     #check for end of table
+  beq   SetSpawn_Exit
   cmpw  r3,r6             #check for matching stage ID
-  addi  r5,r5,1           #inc count
-  bne   StageSearchLoop
-  mr    SpawnTable,r4
+  beq SetSpawn_FoundStage
+  addi  REG_SpawnTable,REG_SpawnTable,EntryLength
+  b SetSpawn_StageSearchLoop
 
-#Get Players Spawn Point and Facing Direction
-  addi SpawnTable,SpawnTable,0x1    #Skip past stage ID
-  lbzx SpawnID,SpawnOrder,SpawnTable
-  addi SpawnTable,SpawnTable,0x4    #Skip past spawn IDs
-  lbzx FacingDirection,SpawnOrder,SpawnTable
-  extsb FacingDirection,FacingDirection
+SetSpawn_FoundStage:
+#Get Players Spawn Point and Facing Direction Pointer
+  addi REG_SpawnTable,REG_SpawnTable,SpawnData      #Skip past stage ID
+  mulli r3,REG_IsTeams,SpawnDataLength
+  add REG_SpawnTable,REG_SpawnTable,r3              #Get to teams/singles data
+  mulli r3,REG_SpawnID,SpawnDataLengthPerPlayer     #Get to this players data
+  add REG_SpawnTable,REG_SpawnTable,r3
 
-#Facing Direction to Float
-  lis	r0, 0x4330
-  lfd	f2, -0x6758 (rtoc)
-  xoris	r3, FacingDirection,0x8000
-  stw	r0,0x40(sp)
-  stw	r3,0x44(sp)
-  lfd	f1,0x40(sp)
-  fsubs	f1,f1,f2		#Convert To Float
-#Store Facing Direction
-  stfs f1,0x40(StaticBlock)
-#Return Spawn Point
-  mr  r3,SpawnID
-  b Exit
+#Set players new spawn coordinates
+  addi  r4,sp,0x80
+  lfs f1,SpawnX(REG_SpawnTable)
+  stfs  f1,0x0(r4)
+  lfs f1,SpawnY(REG_SpawnTable)
+  stfs  f1,0x4(r4)
+  li  r3,0
+  stw  r3,0x8(r4)
+  mr  r3,REG_PlayerSlot
+  branchl r12,0x80032768
 
-######################
-GetSpawnOrder:
-.set LoopCount,31
-.set SpawnOrder,30
-.set PlayerSlot,28
+#Update Facing Direction
+  mr  r3,REG_PlayerSlot
+  lfs f1,SpawnFacing(REG_SpawnTable)
+  branchl r12,0x80033094
 
-backup
-#Init variables
-  mr  PlayerSlot,r3
-  li  SpawnOrder,0
-  li  LoopCount,0
-
-#Start Loop
-GetSpawnOrder_Loop:
-#Load slot type
-  mr  r3,LoopCount
-  branchl r12,PlayerBlock_LoadSlotType
-  cmpwi r3,0x1
-  bgt GetSpawnOrder_IncLoop      #If >1, no player present
-#Player is present, check if this is the one were looking for
-  cmpw PlayerSlot,LoopCount
-  beq GetSpawnOrder_Exit
-#Increment offset
-  addi  SpawnOrder,SpawnOrder,1
-
-GetSpawnOrder_IncLoop:
-  addi LoopCount,LoopCount,1
-  cmpwi LoopCount,4
-  ble GetSpawnOrder_Loop
-
-GetSpawnOrder_Exit:
-  mr  r3,SpawnOrder    #Return spawn order
+SetSpawn_Exit:
   restore
   blr
+#endregion
+
 ######################
 NeutralSpawnTable:
 blrl
+.set  EntryLength,0x4 + SpawnDataLength * 2
+.set  SpawnDataLength,0xC *4
+.set  SpawnDataLengthPerPlayer,0xC
+
+.set  StageID,0x0
+.set  SpawnData,0x4
+  .set  SpawnX,0x0
+  .set  SpawnY,0x4
+  .set  SpawnFacing,0x8
+
 #FD
-.byte 0x20                  #Stage ID
-.byte 0x00,0x01,0x02,0x03   #Nuetral Spawn Index
-.byte 0x01,0xFF,0x01,0xFF   #Facing Directions
+.long 0x20                  #Stage ID
+  #Singles Data
+    .float -60,10,1
+    .float 60,10,-1
+    .float -20,10,1
+    .float 20,10,-1
+  #Teams Data
+    .float -20,10,1
+    .float -60,10,1
+    .float 20,10,-1
+    .float 60,10,-1
 #Battlefield
-.byte 0x1F                  #Stage ID
-.byte 0x02,0x03,0x00,0x01   #Nuetral Spawn Index
-.byte 0x01,0xFF,0xFF,0x01   #Facing Directions
+.long 0x1F                  #Stage ID
+  #Singles Data
+    .float -38.8,35.2,1
+    .float 38.8,35.2,-1
+    .float 0,8,1
+    .float 0,62.4,-1
+  #Teams Data
+    .float -38.8,35.2,1
+    .float -38.8,5,1
+    .float 38.8,35.2,-1
+    .float 38.8,5,-1
 #Yoshi's Story
-.byte 0x08                  #Stage ID
-.byte 0x00,0x01,0x03,0x02   #Nuetral Spawn Index
-.byte 0x01,0xFF,0x01,0xFF   #Facing Directions
+.long 0x08                  #Stage ID
+  #Singles Data
+    .float -42,26.6,1
+    .float 42,28,-1
+    .float 0,46.9,-1
+    .float 0,4.9,1
+  #Teams Data
+    .float -42,26.6,1
+    .float -42,5,1
+    .float 42,28,-1
+    .float 42,5,-1
 #Dream Land
-.byte 0x1C                  #Stage ID
-.byte 0x01,0x03,0x00,0x02   #Nuetral Spawn Index
-.byte 0x01,0xFF,0x01,0xFF   #Facing Directions
+.long 0x1C                  #Stage ID
+  #Singles Data
+    .float -46.6,37.2,1
+    .float 47.4,37.3,-1
+    .float 0,7,1
+    .float 0,58.5,-1
+  #Teams Data
+    .float -46.6,37.2,1
+    .float -46.6,5,1
+    .float 47.4,37.3,-1
+    .float 47.4,5,-1
 #FoD
-.byte 0x02                  #Stage ID
-.byte 0x00,0x01,0x02,0x03   #Nuetral Spawn Index
-.byte 0x01,0xFF,0xFF,0x01   #Facing Directions
+.long 0x02                  #Stage ID
+  #Singles Data
+    .float -41.25,21,1
+    .float 41.25,27,-1
+    .float 0,5.25,-1
+    .float 0,48,1
+  #Teams Data
+    .float -41.25,21,1
+    .float -41.25,5,1
+    .float 41.25,27,-1
+    .float 41.25,5,-1
 #Pokemon Stadium
-.byte 0x03                  #Stage ID
-.byte 0x00,0x01,0x02,0x03   #Nuetral Spawn Index
-.byte 0x01,0xFF,0xFF,0x01   #Facing Directions
+.long 0x03                  #Stage ID
+  #Singles Data
+    .float -40,32,1
+    .float 40,32,-1
+    .float 70,7,-1
+    .float -70,7,1
+  #Teams Data
+    .float -40,32,1
+    .float -40,5,1
+    .float 40,32,-1
+    .float 40,5,-1
 #Terminator
-.byte 0xFF
+.long -1
 .align 2
 ######################
 
-Original:
-restore
-addi	r3, r28, 0
-b Skip
 Exit:
-restore
-Skip:
+  restore
+  lbz	r0, 0x24D0 (r31)
