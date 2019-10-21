519dd25d6134360f9a308ed9fe89566ae9f5e23c
Vin Bertinelli <vinbertinelli@gmail.com>
2019-10-21 06:02:46 -0400

    Update Neutral Spawn.asm

--- ASM/Additional Codes/Neutral Spawn.asm
+++ ASM/Additional Codes/Neutral Spawn.asm
@@ -1,6 +1,7 @@
 #To be inserted at 8016e510
 .include "../Globals.s"
 
+
 .set MatchInfo,31
 .set SpawnTable,30
 .set REG_PlayerSlot,28
@@ -172,7 +173,7 @@ SetSpawn:
 SetSpawn_StageSearchLoop:
   lwz   r3,StageID(REG_SpawnTable)            #get stage ID
   cmpwi r3,-1                     #check for end of table
-  beq   SetSpawn_Exit
+  beq   SetSpawn_NotFound
   cmpw  r3,r6             #check for matching stage ID
   beq SetSpawn_FoundStage
   addi  REG_SpawnTable,REG_SpawnTable,EntryLength
@@ -196,11 +197,53 @@ SetSpawn_FoundStage:
   stw  r3,0x8(r4)
   mr  r3,REG_PlayerSlot
   branchl r12,0x80032768
+  b SetSpawn_UpdateFacingDirection
 
+SetSpawn_NotFound:
+#If singles, use spawn order
+  cmpwi REG_IsTeams,1
+  beq SetSpawn_NotFound_Teams
+  mr  r3,REG_SpawnID
+  b SetSpawn_NotFound_UpdatePosition
+SetSpawn_NotFound_Teams:
+  bl  NeutralSpawnTable_NotFound
+  mflr  r3
+  lbzx  r3,r3,REG_SpawnID
+  b SetSpawn_NotFound_UpdatePosition
+SetSpawn_NotFound_UpdatePosition:
+#Get XYZ from spawn ID
+  addi  r4,sp,0x80
+  branchl r12,0x80224e64
+#Set new XYZ spawn
+  mr  r3,REG_PlayerSlot
+  addi  r4,sp,0x80
+  branchl r12,0x80032768
+  b SetSpawn_UpdateFacingDirection
+
+SetSpawn_UpdateFacingDirection:
+#Load X Position
+  mr  r3,REG_PlayerSlot
+  addi  r4,sp,0x80
+  branchl r12,0x800326cc
+  lfs f1,0x80(sp)
+#Compare to 0
+  lfs	f0, -0x5718 (rtoc)
+  fcmpo cr0,f1,f0
+  ble SetSpawn_UpdateFacingDirection_FacingRight
+SetSpawn_UpdateFacingDirection_FacingLeft:
+  lfs	f1, -0x5708 (rtoc)
+  b SetSpawn_UpdateFacingDirection_Store
+SetSpawn_UpdateFacingDirection_FacingRight:
+  lfs	f1, -0x5734 (rtoc)
+SetSpawn_UpdateFacingDirection_Store:
 #Update Facing Direction
   mr  r3,REG_PlayerSlot
-  lfs f1,SpawnFacing(REG_SpawnTable)
   branchl r12,0x80033094
+
+SetSpawn_AdjustEntryFrames:
+  mr  r3,REG_PlayerSlot
+  mulli r4,REG_SpawnID,5
+  branchl r12,0x80035fdc
 
 SetSpawn_Exit:
   restore
@@ -211,89 +254,94 @@ SetSpawn_Exit:
 NeutralSpawnTable:
 blrl
 .set  EntryLength,0x4 + SpawnDataLength * 2
-.set  SpawnDataLength,0xC *4
-.set  SpawnDataLengthPerPlayer,0xC
+.set  SpawnDataLength,0x8 *4
+.set  SpawnDataLengthPerPlayer,0x8
 
 .set  StageID,0x0
 .set  SpawnData,0x4
   .set  SpawnX,0x0
   .set  SpawnY,0x4
-  .set  SpawnFacing,0x8
 
 #FD
 .long 0x20                  #Stage ID
   #Singles Data
-    .float -60,10,1
-    .float 60,10,-1
-    .float -20,10,1
-    .float 20,10,-1
+    .float -60,10
+    .float 60,10
+    .float -20,10
+    .float 20,10
   #Teams Data
-    .float -20,10,1
-    .float -60,10,1
-    .float 20,10,-1
-    .float 60,10,-1
+    .float -20,10
+    .float -60,10
+    .float 20,10
+    .float 60,10
 #Battlefield
 .long 0x1F                  #Stage ID
   #Singles Data
-    .float -38.8,35.2,1
-    .float 38.8,35.2,-1
-    .float 0,8,1
-    .float 0,62.4,-1
+    .float -38.8,35.2
+    .float 38.8,35.2
+    .float 0,8
+    .float 0,62.4
   #Teams Data
-    .float -38.8,35.2,1
-    .float -38.8,5,1
-    .float 38.8,35.2,-1
-    .float 38.8,5,-1
+    .float -38.8,35.2
+    .float -38.8,5
+    .float 38.8,35.2
+    .float 38.8,5
 #Yoshi's Story
 .long 0x08                  #Stage ID
   #Singles Data
-    .float -42,26.6,1
-    .float 42,28,-1
-    .float 0,46.9,-1
-    .float 0,4.9,1
+    .float -42,26.6,
+    .float 42,28
+    .float 0,46.9
+    .float 0,4.9
   #Teams Data
-    .float -42,26.6,1
-    .float -42,5,1
-    .float 42,28,-1
-    .float 42,5,-1
+    .float -42,26.6
+    .float -42,5
+    .float 42,28
+    .float 42,5
 #Dream Land
 .long 0x1C                  #Stage ID
   #Singles Data
-    .float -46.6,37.2,1
-    .float 47.4,37.3,-1
-    .float 0,7,1
-    .float 0,58.5,-1
+    .float -46.6,37.2
+    .float 47.4,37.3
+    .float 0,7
+    .float 0,58.5
   #Teams Data
-    .float -46.6,37.2,1
-    .float -46.6,5,1
-    .float 47.4,37.3,-1
-    .float 47.4,5,-1
+    .float -46.6,37.2
+    .float -46.6,5
+    .float 47.4,37.3
+    .float 47.4,5
 #FoD
 .long 0x02                  #Stage ID
   #Singles Data
-    .float -41.25,21,1
-    .float 41.25,27,-1
-    .float 0,5.25,-1
-    .float 0,48,1
+    .float -41.25,21
+    .float 41.25,27
+    .float 0,5.25
+    .float 0,48
   #Teams Data
-    .float -41.25,21,1
-    .float -41.25,5,1
-    .float 41.25,27,-1
-    .float 41.25,5,-1
+    .float -41.25,21
+    .float -41.25,5
+    .float 41.25,27
+    .float 41.25,5
 #Pokemon Stadium
 .long 0x03                  #Stage ID
   #Singles Data
-    .float -40,32,1
-    .float 40,32,-1
-    .float 70,7,-1
-    .float -70,7,1
+    .float -40,32
+    .float 40,32
+    .float 70,7
+    .float -70,7
   #Teams Data
-    .float -40,32,1
-    .float -40,5,1
-    .float 40,32,-1
-    .float 40,5,-1
+    .float -40,32
+    .float -40,5
+    .float 40,32
+    .float 40,5
 #Terminator
 .long -1
+.align 2
+
+NeutralSpawnTable_NotFound:
+blrl
+#Doubles
+.long 0x00030102
 .align 2
 ######################
 
