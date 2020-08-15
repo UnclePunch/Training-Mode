b30a641c73ca152280420afc5d716a57aee11c78
UnclePunch <vinbertinelli@gmail.com>
2020-08-15 19:36:03 -0400

    tm: update build script

--- tmdata/build.bat
+++ tmdata/build.bat
@@ -1,4 +1,6 @@
-"../../MexTK/MexTK.exe" -ff -i "source/events.c" -s tmFunction -o "output/TmDt.dat" -t "../../MexTK/tmFunction.txt" -q -ow -w -c -op 1
-"../../MexTK/MexTK.exe" -addSymbol "output/TmDt.dat" "assets/evMenu.dat" -ow
+cd "%MEX_REPO%\patch\tmdata"
+xcopy /s /y "assets\evMenu.dat" "output/TmDt.dat"
+"../../MexTK/MexTK.exe" -ff -i "source/events.c" -s tmFunction -dat "output/TmDt.dat" -t "../../MexTK/tmFunction.txt" -q -ow -w -c -op 1
 "../../MexTK/MexTK.exe" -trim "output/TmDt.dat"
+
 pause
