71e0c77f7050eb8bb13b9d6bcd77976475627e5f
UnclePunch <vinbertinelli@gmail.com>
2020-11-01 01:59:38 -0500

    adjust event C build scripts

--- patch/events/lab/build.bat
+++ patch/events/lab/build.bat
@@ -1,3 +1,3 @@
-"../../../MexTK/MexTK.exe" -ff -i "source\lab.c" -s evFunction -o "output/EvLab.dat" -t "../../../MexTK/evFunction.txt" -q -ow -w -c -op 1
+"../../../MexTK/MexTK.exe" -ff -i "source\lab.c" -s evFunction -o "output/EvLab.dat" -t "../../../MexTK/evFunction.txt" -q -ow -w -c -l "../../../MexTK/melee.link" -op 1
 "../../../MexTK/MexTK.exe" -trim "output/EvLab.dat"
 pause
--- patch/tmdata/build.bat
+++ patch/tmdata/build.bat
@@ -1,5 +1,5 @@
 xcopy /s /y "assets\evMenu.dat" "output/TmDt.dat"
-"../../MexTK/MexTK.exe" -ff -i "source/events.c" -s tmFunction -dat "output/TmDt.dat" -t "../../MexTK/tmFunction.txt" -q -ow -w -c -op 1
+"../../MexTK/MexTK.exe" -ff -i "source/events.c" -s tmFunction -dat "output/TmDt.dat" -t "../../MexTK/tmFunction.txt" -q -ow -w -c -l "../../MexTK/melee.link" -op 1
 "../../MexTK/MexTK.exe" -trim "output/TmDt.dat"
 
 pause
