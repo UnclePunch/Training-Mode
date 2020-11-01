707cafba441fc7255ba32a226cf423053fe8142d
UnclePunch <vinbertinelli@gmail.com>
2020-11-01 02:50:10 -0500

    move UCF

--- patch/events/lab/build.bat
+++ patch/events/lab/build.bat
@@ -1,3 +1,8 @@
-"../../../MexTK/MexTK.exe" -ff -i "source\lab.c" -s evFunction -o "output/EvLab.dat" -t "../../../MexTK/evFunction.txt" -q -ow -w -c -l "../../../MexTK/melee.link" -op 1
+xcopy /s /y "assets\labData.dat" "output/EvLab.dat"
+"../../../MexTK/MexTK.exe" -ff -i "source/lab.c" -s evFunction -dat "output/EvLab.dat" -t "../../../MexTK/evFunction.txt" -q -ow -w -c -l "../../../MexTK/melee.link" -op 1
 "../../../MexTK/MexTK.exe" -trim "output/EvLab.dat"
+
+xcopy /s /y "assets\importData.dat" "output/EvLabCSS.dat"
+"../../../MexTK/MexTK.exe" -ff -i "source/lab_css.c" -s cssFunction -dat "output/EvLabCSS.dat" -t "../../../MexTK/cssFunction.txt" -q -ow -w -c -l "../../../MexTK/melee.link" -op 1
+"../../../MexTK/MexTK.exe" -trim "output/EvLabCSS.dat"
 pause
