2a903c378dd90ef85832e512f2282c80fe195ba0
UnclePunch <vinbertinelli@gmail.com>
2020-12-06 07:49:52 -0500

    tm: update menu inputs during engine pause
    doing this to make the menu inputs work with the TM menu while the engine is paused. not sure if there are any side effects to doing this but it seems to work??

---
+++ ASM/Additional Codes/Pad - Update During Frame Advance.asm
@@ -0,0 +1,7 @@
+#To be inserted at 802641B0
+
+# doing this to make the menu inputs work with the TM menu
+# while the engine is paused. not sure if there are any
+# side effects to doing this but it seems to work??
+
+nop
